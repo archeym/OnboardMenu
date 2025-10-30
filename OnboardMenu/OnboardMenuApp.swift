467ad105acb507e56315a7893b931cebcc5d917c
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2025-10-30 09:18:24 +0100

    Initial Commit

---
+++ OnboardMenu/OnboardMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardMenuApp.swift
+//  OnboardMenu
+//
+//  Created by Arkadijs Makarenko on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct OnboardMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnboardMenuTests/OnboardMenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardMenuTests.swift
+//  OnboardMenuTests
+//
+//  Created by Arkadijs Makarenko on 30/10/2025.
+//
+
+import Testing
+@testable import OnboardMenu
+
+struct OnboardMenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
